2c3f481f59ad7fcf0c160acf92e44d32760da1cc
qchea <qchea@amazon.com>
2021-05-25 14:16:40 -0500

    MAINT: unused test utilities
    Signed-off-by: qchea <qchea@amazon.com>

--- exporter/dataprepperexporter/testdata/common.go
+++ exporter/dataprepperexporter/testdata/common.go
@@ -7,21 +7,6 @@ var (
 	spanEventAttributes = map[string]pdata.AttributeValue{"span-event-attr": pdata.NewAttributeValueString("span-event-attr-val")}
 )
 
-const (
-	TestLabelKey1       = "label-1"
-	TestLabelValue1     = "label-value-1"
-	TestLabelKey2       = "label-2"
-	TestLabelValue2     = "label-value-2"
-)
-
-func initMetricLabels1(dest pdata.StringMap) {
-	dest.InitFromMap(map[string]string{TestLabelKey1: TestLabelValue1})
-}
-
-func initMetricLabels2(dest pdata.StringMap) {
-	dest.InitFromMap(map[string]string{TestLabelKey2: TestLabelValue2})
-}
-
 func initResourceAttributes1(dest pdata.AttributeMap) {
 	dest.InitFromMap(resourceAttributes1)
 }
